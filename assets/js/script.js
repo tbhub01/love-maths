b84698fc7683c59ce22c61a25c8869eaf4542a29
Tola  Buluro <tbuluro@yahoo.com>
2024-07-22 12:28:52 +0000

    adding functions template

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame () {
+
+}
+
+function checkAnswer () {
+    
+}
+
+function calculatecorrectAnswer () {
+    
+}
+
+function incrementScore () {
+    
+}
+
+function incrementwrongAnswer () {
+    
+}
+
+function displayAdditionQuestion () {
+    
+}
+
+function displaySubtractQuestion () {
+    
+}
+
+function displayMultiplyQuestion () {
+    
+}
